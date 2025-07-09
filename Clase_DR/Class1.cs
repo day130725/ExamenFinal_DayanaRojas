5afa65f2203ec78e16b15ff28fc66c25841b57b9
day130725 <N00439638@upn.pe>
2025-07-09 18:07:23 -0500

    segundo cambio

--- Clase_DR/Class1.cs
+++ Clase_DR/Class1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Clase_DR
@@ -29,8 +30,27 @@ namespace Clase_DR
             "Cuadro", "Espejo", "Reloj de pared", "Lámpara", "Cortina",
             "Biblia", "Libro de cocina", "Diccionario", "Enciclopedia", "Novela",
         };
-        string[] articulos;
-        int[,] ventas;
-        int[,] totalArticulo;
+        string[] Articulos;
+        int[,] Ventas;
+        int[,] TotalArticulo;
+        int[,] VentaMinima;
+        int[,] VentaMaxima;
+        double[] Promedio;
+        int[] SumaPorDia;
+        Random r = new Random();
+
+        public void GenerarArticulos()
+        {
+            Console.Write("Ingrese la cantidad de artículos: ");
+            int cantidad = int.Parse(Console.ReadLine());
+            Articulos = new string[cantidad];
+            for (int i = 0; i < cantidad; i++)
+            {
+                int numero = r.Next(dic_articulos.Length);
+                Articulos[i] = dic_articulos[numero];
+                Thread.Sleep(20);
+            }
+        }
     }
 }
+
