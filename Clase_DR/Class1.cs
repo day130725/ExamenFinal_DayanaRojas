d3e8957bd389170556db1547a1ec22f793fb93bc
day130725 <N00439638@upn.pe>
2025-07-09 17:23:05 -0500

    Agregar archivos de proyecto.

---
+++ Clase_DR/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_DR
+{
+    public class Class1
+    {
+        string[] dic_articulos = new string[]
+        {
+            "Detergente", "Pan", "Arroz", "Fideos", "Salchichas",
+            "Carne", "Pollo", "Pescado", "Verduras", "Frutas",
+            "Lapiz", "Cuaderno", "Libro", "Borrador", "Tijeras",
+            "Juguete", "Ropa", "Zapatos", "Perfume", "Jabon",
+            "Champu", "Acondicionador", "Colinos", "Desodorante", "Maquillaje",
+            "Pegamento", "Floder", "Calculadora", "Regla", "Marcador",
+            "Cinta adhesiva", "Grapadora", "Tijeras", "Papel higienico", "Toallas de papel",
+            "Escuadra", "Transportador", "Pincel", "Paletas", "Lienzo",
+            "Cartuchera", "Agenda", "Calendario", "Organizador", "Bolsa de Tela",
+            "Caja de herramientas", "Taladro", "Destornillador", "Martillo", "Sierra",
+            "Batidora", "Tostadora", "Microondas", "Horno", "Refrigerador",
+            "Tomatodo", "Termo", "Bidon", "Exprimidor", "Licuadora",
+            "Cuchillo de cocina", "Tabla de cortar", "Colador", "Rallador", "Abrelatas",
+            "Cuchara de palo", "Mortero", "Pimentero", "Salero", "Azucarera",
+            "Candelabro", "Portavelas", "Vela aromática", "Incienso", "Ambientador",
+            "Maceta", "Planta", "Flor", "Jarrón", "Adorno",
+            "Cuadro", "Espejo", "Reloj de pared", "Lámpara", "Cortina",
+            "Biblia", "Libro de cocina", "Diccionario", "Enciclopedia", "Novela",
+        };
+        string[] articulos;
+        int[,] ventas;
+        int[,] totalArticulo;
+    }
+}
